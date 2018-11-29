7fc5b890e1f23a78d4ff30d9e3d1e31028d7d7a5
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2018-11-28 17:06:31 -0800

    added links, routes and start of sign in auth

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { userSignIn, userSignOut } from '../actions';
 
 class Nav extends Component {
+    renderLinks(){
+        const { auth, signIn, signOut } = this.props;
+
+        if ( auth ){
+            return <button onClick={signOut} className="yellow btn darken-2">Sign Out</button>
+        }
+
+        return <button onClick={signIn} className="btn red darken-2">Sign In</button>
+    }
+    
     render(){
         const navStyle = {
             padding: '0 8px'
         }
 
-
         return (
-            <nav style={navStyle} className="deep-purple accent-1">
+            <nav style={navStyle} className="orange darken-2">
                 <div className="nav-wrapper">
                     <Link to="/" className="brand-logo">Movie Quotes</Link>
 
@@ -30,7 +41,7 @@ class Nav extends Component {
                             <Link to="/quotes">Quotes</Link>
                         </li>
                         <li>
-                            <Link to="/sign-in">Sign In</Link>
+                            {this.renderLinks()}
                         </li>
                         <li>
                             <Link to="/sign-up">Sign Up</Link>
@@ -42,4 +53,13 @@ class Nav extends Component {
     }
 }
 
-export default Nav;
+function mapStateToProps(state){
+    return {
+        auth: state.user.auth
+    }
+}
+
+export default connect(mapStateToProps, {
+    signIn: userSignIn,
+    signOut: userSignOut
+})(Nav);
