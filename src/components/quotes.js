e32f0f4ed7901289ab6ef3e60163d9aa292f1dbf
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2018-11-28 18:08:16 -0800

    added higher order component that handles basic user authentication

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import auth from '../hoc/auth';
 
-export default props => (
-    <div>
-        <h1 className="center">Movie Quote</h1>
-        <h5>Pu$&ys on the pavement fellas!</h5>
-    </div>
-)
+class Quotes extends Component {
+    
+    render(){
+        return (
+            <div>
+                <h1 className="center">Movie Quote</h1>
+                <h5>Pu$&ys on the pavement fellas!</h5>
+            </div>
+        );
+    }
+}
+
+export default auth(Quotes);
+
--- src/components/secret_list.js
+++ src/components/secret_list.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import auth from '../hoc/auth';
 
-export default props => (
-    <div>
-        <h1 className="center">Secret Ops Que</h1>
-        <ol>
-            <li>Brad</li>
-            <li>Kurt</li>
-            <li>Jermaine</li>
-            <li>Frank</li>
-            <li>Miguel</li>
-            <li>Rita</li>
-            <li>Christina</li>
-        </ol>
-    </div>
-)
+const SecretList = props => {
+
+    return(
+        <div>
+            <h1 className="center">Secret Ops Que</h1>
+            <ol>
+                <li>Brad</li>
+                <li>Kurt</li>
+                <li>Jermaine</li>
+                <li>Frank</li>
+                <li>Miguel</li>
+                <li>Rita</li>
+                <li>Christina</li>
+            </ol>
+        </div>
+    );
+}
+export default auth(SecretList);
